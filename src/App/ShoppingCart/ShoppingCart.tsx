16d67438de50c7a207e3fbc9542e204b5c388d25
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-07-25 20:02:40 +1000

    feat(ShoppingCart): introducing the ShoppingCart component

---
+++ src/App/ShoppingCart/ShoppingCart.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from './ShoppingCart.module.scss';
+
+interface Props {}
+
+interface State {
+  isOpen: boolean;
+}
+
+class ShoppingCart extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  handleClick = (e) => {
+    this.setState({isOpen: !this.state.isOpen})
+  }
+
+  render() {
+    return (
+      <div className={styles.cartContainer}>
+        <button 
+          className={styles.button}
+          onClick={this.handleClick}
+        >
+          Shopping Cart
+        </button>
+        {this.state.isOpen && (
+          <div className={styles.cartDropDown}>
+            <ul>
+              <li>robot 1</li>
+              <li>robot 2</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default ShoppingCart;
