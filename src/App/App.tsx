87046457d673e0b9d0df0780cfe3ce62e66e29f6
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-07-25 20:32:21 +1000

    update(App): updating the asynchronous APi request data replacing mockdata, and using componentDidMount and promise to handle

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,15 +4,36 @@ import robots from '../mockdata/robots.json';
 import styles from './App.module.css';
 import ShoppingCart from './ShoppingCart/ShoppingCart';
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robots.map((r) => <Robot id={r.id} email={r.email} name={r.name} />)}
-      </div>      
-    </div>
-  );
+interface Props {}
+
+interface State {
+  robotGallery: any
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      robotGallery: [],
+    };
+  }
+
+  componentDidMount () {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => this.setState({ robotGallery: data }));
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map((r) => <Robot id={r.id} email={r.email} name={r.name} />)}
+        </div>      
+      </div>
+    );
+  }
 }
 
 export default App;
