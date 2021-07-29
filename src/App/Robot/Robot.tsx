2871b57b5c8dc8c543bb7cfd0efd2aea71cdff9e
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-07-29 20:30:53 +1000

    feat(appState): introducing the Context Api of appState to Robot and ShoppingCart components

--- src/App/Robot/Robot.tsx
+++ src/App/Robot/Robot.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from './Robot.module.css';
+import { appContext, appSetStateContext } from '../../Context/appState';
 
 interface RobotProps {
   id: number,
@@ -8,11 +9,30 @@ interface RobotProps {
 }
 
 const Robot : React.FC<RobotProps> = ({ id, name, email }) => {
+  const value = useContext(appContext);
+  const setState = useContext(appSetStateContext);
+
+  const addToCart = () => {
+    if(setState) {
+      setState((preState) => {
+        return {
+          ...preState,
+          shoppingCart: {
+            items: [...preState.shoppingCart.items, {id, name}]
+          }
+        }
+      })
+    }
+
+  };
+
   return (
     <div className={styles.cardContainer}>
       <img alt='robot' src={`https://robohash.org/${id}`} />
       <h2>{name}</h2>
       <p>{email}</p>
+      <p>{value.userName}</p>
+      <button onClick={addToCart}>Add To Cart</button>
     </div>
   )
 }
